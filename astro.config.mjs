580df932e0e30bf20e60b57926b0ed8111d7799c
Tyson Sukeforth <3186054+tybot3k@users.noreply.github.com>
2025-03-18 19:46:32 -0400

    POR-16 Workaround for Astro Cloudflare integration bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,14 @@ export default defineConfig({
   integrations: [react()],
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
   },
 
   adapter: cloudflare()
